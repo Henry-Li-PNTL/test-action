e306e1e7d90448276a3ca6268a65b40be9f138e2
Henry-Li-PNTL <136312315+Henry-Li-PNTL@users.noreply.github.com>
2023-07-24 10:30:53 +0800

    Update action.py

--- action.py
+++ action.py
@@ -10,7 +10,7 @@ raw_branch = sys.argv[1]
 
 # 用您自己的 GitHub Token 初始化 Github 对象
 github_token = "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"
-g = Github(Auth(github_token))
+g = Github(Auth.Token(github_token))
 
 # 您的 GitHub 仓库的所有 Pull Requests
 repo = g.get_repo("Henry-Li-PNTL/test-action")
