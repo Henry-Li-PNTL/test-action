c67699e51ec3ba44054e41d8eaa4359ff4c5eb35
Henry-Li-PNTL <136312315+Henry-Li-PNTL@users.noreply.github.com>
2023-07-24 11:15:34 +0800

    Update action.py

--- action.py
+++ action.py
@@ -1,20 +1,60 @@
-from github import Github
-from github import Github
-
-# Authentication is defined via github.Auth
-from github import Auth
+import requests
+from github import Auth, Github
 import sys
+from pprint import pprint
 
-raw_branch = sys.argv[1]
+# raw_branch = sys.argv[1]
 
 
 # 用您自己的 GitHub Token 初始化 Github 对象
-github_token = "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"
-g = Github(Auth.Token(github_token))
+github_token = "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"
+
+
+
+r = requests.get(
+    "https://api.github.com/repos/Henry-Li-PNTL/test-action",
+    headers= {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {github_token}",
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+)
+r.raise_for_status()
+# pprint(r.json())
+
+
+
+r = requests.get(
+    "https://api.github.com/repos/Henry-Li-PNTL/test-action/pulls/5",
+    headers= {
+        "Accept": "application/vnd.github.v3+json",
+        "Authorization": f"Bearer {github_token}",
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+)
+
+# pprint(r.json())
+
+
+print(f"Merge {r.json()['head']['ref']} to {r.json()['base']['ref']}")
+
+exit()
+
+
+
+
+
+
+
+
+# ====================================================================================
+auth = Auth.Token("access_token")
+g = Github(auth=auth)
 
 # 您的 GitHub 仓库的所有 Pull Requests
 repo = g.get_repo("Henry-Li-PNTL/test-action")
-pull_number = int( raw_branch.split("/")[2] )
+pull_number = 5
+# pull_number = int( raw_branch.split("/")[2] )
 # pull_number = 42  # 用实际的 Pull Request 编号替换这里
 
 # 获取指定 Pull Request 的信息
@@ -26,3 +66,4 @@ source_branch = pull_request.head.ref
 
 print(f"Pull Request #{pull_number} 合并的分支是: {base_branch}")
 print(f"Pull Request #{pull_number} 来源分支是: {source_branch}")
+# ====================================================================================
